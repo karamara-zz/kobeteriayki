9b903e3c0cf30f7d181eb869d7a7bbb76f9435da
Sung Kang <sungjk@msn.com>
2018-02-18 01:24:08 -0800

    trying to fix

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ var path = require('path');
 app.use(express.static(path.join(__dirname, "./client")));
 
 var server = app.listen(8000, function(){
-	console.log('listening local host 8000')
+	console.log('listening local host 8000');
 })

11605d1c4f332c03f3106c03f2649f322ca46d82
Sung Kang <sungjk@msn.com>
2018-02-18 02:18:56 -0800

    changed port number again

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname, "./client")));
 
-var server = app.listen(8000, function(){
-	console.log('listening local host 8000');
+var server = app.listen(3000, function(){
+	console.log('listening local host 3000');
 })

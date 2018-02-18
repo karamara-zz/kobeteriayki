b6c2f1e53bf677a8d4c0be04d9e15b2d1dafd3c8
Sung Kang <sungjk@msn.com>
2018-02-18 03:10:17 -0800

    updating

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
+var path = require('path');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, "./client")));
+app.use(express.static(path.join(__dirname, "./views")));
+
+var server = app.listen(8000, function(){
+	console.log('listening local host 8000')
+})

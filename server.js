21783d7d3977783461e91d6332211cc4d621e39c
Sung Kang <sungjk@msn.com>
2018-02-18 02:33:25 -0800

    deleted all

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-
-app.use(express.static(path.join(__dirname, "./client")));
-
-var server = app.listen(3000, function(){
-	console.log('listening local host 3000');
-})
